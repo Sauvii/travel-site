eddbfb370e6dfeadb661013fb2a7057758e1db47
Saurabh Kamboj <saukambo@publicisgroupe.net>
2022-02-24 17:43:56 +0530

    Header Complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
-const path = require("path")
+const path = require('path')
 
 const postCSSPlugins = [
-  require("postcss-import"),
+  require('postcss-import'),
   require('postcss-mixins'),
-  require("postcss-simple-vars"),
-  require("postcss-nested")
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  // If in the future the creator of the postcss-hexrgba package
+  // releases an update (it is version 2.0.1 as I'm writing this)
+  // then it will likely work with PostCSS V8 so you can uncomment
+  // the line below and also install the package with npm.
+  //require('postcss-hexrgba'),
+  require('autoprefixer')
 ]
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: './app/assets/scripts/App.js',
   output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
   },
   devServer: {
     before: function(app, server) {
@@ -22,7 +28,7 @@ module.exports = {
     port: 3000,
     host: '0.0.0.0'
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
